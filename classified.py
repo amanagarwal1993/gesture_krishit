f5be4849b06abe5ffcd85d902b80609b26aedda5
Aman Agarwal <yuying@protonmail.ch>
2018-06-13 11:35:56 +0530

    Training code added

--- classified.py
+++ classified.py
@@ -1,8 +1,3 @@
-from __future__ import print_function
-#from azure.cognitiveservices.vision.customvision.training import training_api
-#from azure.cognitiveservices.vision.customvision.training.models import ImageUrlCreateEntry
-
-
 import cv2
 import numpy as np
 import copy
@@ -11,59 +6,35 @@ import os
 import requests
 import operator
 
+from keras.models import load_model
+model = load_model('shape-128-grayscale-june11-withflip.h5')
 
-# Replace with a valid key
-training_key = "[]"
-prediction_key = "[]"
+gestures = {0: 'Closed fist', 
+            1: 'Four fingers', 
+            2: 'C shape', 
+            3: 'Number One', 
+            4: 'One finger pointing', 
+            5: 'Pee', 
+            6: 'Two beers', 
+            7: 'Spidey'}
 
 
-# Variables
-_url = '[]'
-
-_maxNumRetries = 10
-
-
-
-def classify():
-        data= None
-        
-        # Change path
-        pathToFileInDisk = r'C:\Users\Krishit Arora\Desktop\1.jpg'
-        imgt=cv2.imread(pathToFileInDisk,0)
-        
-        #cv2.imshow('image', imgt)
-        
-        # Computer Vision parameters
-        params = { 'visualFeatures' : 'Color,Categories'} 
-        
-        headers = dict()
-        headers['Prediction-Key'] = prediction_key
-        headers['Content-Type'] = 'application/octet-stream'
-        
-        response   = requests.post(_url, 
-                                   headers=headers, 
-                                   params=params, 
-                                   data=data)
-        
-        response.raise_for_status()
-        
-        analysis      = response.json()
-        print(str(analysis["Predictions"][0]["Tag"])+'\t'+str(analysis["Predictions"][0]["Probability"]))
-        cv2.imshow(str(analysis["Predictions"][0]["Tag"]),imgt)
-        print (analysis)
-        
+def classify(img):
+    image = np.array([img * 1./255 - 0.5])
+    out = model.predict(image)
+    gesture = gestures[np.argmax(out)]
+    if np.max(out) > 0.40:
+        return gesture
+    else:
+        return "Not clear"
 
 # parameters
-cap_region_x_begin=0.5  # start point/total width
-cap_region_y_end=0.8  # start point/total width
 threshold = 60  #  BINARY threshold
-blurValue = 41  # GaussianBlur parameter
 bgSubThreshold = 50
 
 # variables
 isBgCaptured = 0   # bool, whether the background captured
 triggerSwitch = False  # if true, keyborad simulator works
-i=0
 
 def printThreshold(thr):
     print("! Changed threshold to "+str(thr))
@@ -80,104 +51,47 @@ def removeBG(frame):
     return res
 
 
-def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
-    #  convexity defect
-    hull = cv2.convexHull(res, returnPoints=False)
-    if len(hull) > 3:
-        defects = cv2.convexityDefects(res, hull)
-        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
-
-            cnt = 0
-            for i in range(defects.shape[0]):  # calculate the angle
-                s, e, f, d = defects[i][0]
-                start = tuple(res[s][0])
-                end = tuple(res[e][0])
-                far = tuple(res[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    cnt += 1
-                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-            return True, cnt
-    return False, 0
-
-
 # Camera
 camera = cv2.VideoCapture(0)
 camera.set(10,200)
-cv2.namedWindow('trackbar')
-cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+
+while camera.isOpened():
+    ret, frame = camera.read()
+
+    #frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+
+    # Draw the region of interest onto original frame for visual feedback
+    cv2.rectangle(frame, ((frame.shape[1]-400), 0),
+                 (frame.shape[1], 400), (255, 0, 0), 2)
+    #  Main operation
+    # this part will run when you press 'b' on a frame
+    # Process Image
+    #img = removeBG(frame)
+
+    # extract the ROI
+    roi = frame[0:400, (frame.shape[1]-400):frame.shape[1]]
+    #roi = removeBG(roi)
+    roi = cv2.resize(roi, (128,128), interpolation = cv2.INTER_AREA)
+    
+    # convert the image into binary image
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.flip(gray, 1)
+    #gesture = classify(gray)
+
+    thresh = np.dstack((gray, gray, gray))
+
+    # Overlay
+    frame[0:128, (frame.shape[1]-128):frame.shape[1]] = thresh
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, classify(gray), (10,500), font, 4,(0,0,255),4,cv2.LINE_AA)
+    
+    cv2.imshow('original', frame)
 
 
-while i<1:
-    while camera.isOpened():
-        ret, frame = camera.read()
-        
-        threshold = cv2.getTrackbarPos('trh1', 'trackbar')
-        
-        frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
-        frame = cv2.flip(frame, 1)  # flip the frame horizontally
-        
-        # Draw the region of interest onto original frame for visual feedback
-        cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
-                     (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
-        if isBgCaptured == 0:
-            cv2.imshow('original', frame)
-
-        #  Main operation
-        # this part will run when you press 'b' on a frame
-        if isBgCaptured == 1:
-            # Process Image
-            #img = removeBG(frame)
-            
-            # extract the ROI
-            roi = frame[0:int(cap_region_y_end * frame.shape[0]),
-                        int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]
-            
-            roi = removeBG(roi)
-            #cv2.imshow('mask', img)
-            # convert the image into binary image
-            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-            #cv2.imshow('gray', gray)
-            #blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
-            #cv2.imshow('blur', blur)
-            #ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
-            
-            thresh = np.dstack((gray, gray, gray))
-            
-            # Overlay
-            frame[0:int(cap_region_y_end * frame.shape[0]),int(cap_region_x_begin * frame.shape[1]):frame.shape[1]] = thresh
-            
-            cv2.imshow('original', frame)
-            
-            
-        # Keyboard OP
-        k = cv2.waitKey(10)
-        if k == 27:  # press ESC to exit
-            cv2.destroyAllWindows()
-            break
-        
-        if k == ord('b'):  # press 'b' to capture the background
-            bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
-            isBgCaptured = 1
-            print ('!!!Background Captured!!!')
-            i=i+1
-
-        #elif k == ord('c'):
-            #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            #cv2.imwrite('1.jpg', img_gray)
-            #classify()        
-
-        elif k == ord('r'):  # press 'r' to reset the background
-            bgModel = None
-            triggerSwitch = False
-            isBgCaptured = 0
-            i=0
-            print ('!!!Reset BackGround!!!')
-            
-        elif k == ord('n'):
-            triggerSwitch = True
-            print ('!!!Trigger On!!!')
-
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit
+        camera.release()
+        cv2.destroyAllWindows()
+        break
---
+++ train.py
@@ -0,0 +1,166 @@
+import numpy as np
+import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import glob
+
+from keras import backend as K
+K.set_learning_phase(0)
+
+import tensorflow as tf
+
+# Set up the data
+gestures = ['a', 'b', 'c', 'd', 'g', 'h', 'i', 'l', 'v', 'y']
+
+y_all = []
+paths = []
+
+# Add filepaths to array
+prefix = 'Triesch/*'
+suffix = '.pgm'
+
+for char in gestures:
+    paths_char = []
+    for i in range(1,4):
+        paths_char.extend(glob.glob(prefix + char + str(i) + suffix))
+    if (char == 'h'):
+        char = 'g'
+    elif (char == 'l'):
+        char = 'd'
+    y_all.extend(([char] * len(paths_char)))
+    paths.extend(paths_char)
+
+paths = np.array(paths)
+
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from keras.utils import to_categorical
+
+# Encode string labels into integer format
+enc = LabelEncoder()
+y_all = enc.fit_transform(y_all)
+
+# Convert to one-hot
+y = to_categorical(y_all, num_classes=None)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, y_train, y_val = train_test_split(paths, y, test_size=0.2, random_state=42)
+
+x_val = []
+for path in X_val:
+    x_val.append(mpimg.imread(path))
+x_val = np.array(x_val)
+
+# Initialize the train generator to generate augmented batches of data
+from sklearn.utils import shuffle
+rows = 128
+cols = 128
+
+def generator(paths, labels, zoom=True, warp=True, rotation=20):
+    R1 = cv2.getRotationMatrix2D((cols/2,rows/2),rotation,1)
+    R2 = cv2.getRotationMatrix2D((cols/2,rows/2),rotation*-1,1)
+    
+    R_zoom = cv2.getRotationMatrix2D((cols/2,rows/2),rotation,1.5)
+    R_out = cv2.getRotationMatrix2D((cols/2,rows/2),rotation,0.5)
+    
+    pts1 = np.float32([[30,30],[30,108],[108,30],[108,108]])
+    pts2 = np.float32([[0,0],[0,128],[100,50],[100,100]])
+    pts3 = np.float32([[50,50],[50,100],[128,0],[128,128]])
+                      
+    zoom_left = cv2.getPerspectiveTransform(pts1,pts2)
+    zoom_right = cv2.getPerspectiveTransform(pts1,pts3)
+    
+    # Batches of 14 images
+    for i in range(len(paths)):
+        # Initialize empty batch to be filled in
+        batch_images = []
+        batch_labels = np.array([labels[i]] * 14)
+
+        image = mpimg.imread(paths[i])
+    
+        # Flip
+        batch_images.append(image)
+        flip = (cv2.flip(image, 1))
+        batch_images.append(flip)
+        
+        # Rotate and zoom
+        batch_images.append(cv2.warpAffine(image,R1,(cols,rows)))
+        batch_images.append(cv2.warpAffine(image,R2,(cols,rows)))
+        
+        batch_images.append(cv2.warpAffine(flip,R1,(cols,rows)))
+        batch_images.append(cv2.warpAffine(flip,R2,(cols,rows)))
+        
+        batch_images.append(cv2.warpAffine(image,R_zoom,(cols,rows)))
+        batch_images.append(cv2.warpAffine(image,R_out,(cols,rows)))
+        
+        batch_images.append(cv2.warpAffine(flip,R_zoom,(cols,rows)))
+        batch_images.append(cv2.warpAffine(flip,R_out,(cols,rows)))
+        
+        # Shear
+        batch_images.append(cv2.warpPerspective(image, zoom_left,(cols,rows)))
+        batch_images.append(cv2.warpPerspective(image, zoom_right,(cols,rows)))
+        
+        batch_images.append(cv2.warpPerspective(flip, zoom_left,(cols,rows)))
+        batch_images.append(cv2.warpPerspective(flip, zoom_right,(cols,rows)))
+        
+        batch_images = np.array(batch_images)
+        
+        yield shuffle(batch_images, batch_labels)
+
+train_generator = generator(X_train, y_train)
+
+
+# Create the model
+from keras.models import Sequential
+import keras.layers
+
+model = Sequential()
+
+model.add(keras.layers.Reshape(input_shape=(128, 128), target_shape=(128,128,1)))
+model.add(keras.layers.Conv2D(input_shape=(128,128,1), filters=32, kernel_size=3, strides=1, activation='tanh', kernel_initializer='glorot_uniform'))
+model.add(keras.layers.Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.MaxPooling2D())
+
+model.add(keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.MaxPooling2D())
+
+model.add(keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.MaxPooling2D())
+
+model.add(keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same', activation="tanh", kernel_initializer='glorot_uniform'))
+model.add(keras.layers.MaxPooling2D())
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(8, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer="sgd", metrics=['accuracy'])
+
+
+# Create callback functions for training, to save best models
+from keras.callbacks import ModelCheckpoint, Callback
+
+save_model = keras.callbacks.ModelCheckpoint("shape-128-augmented-june13.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+
+class SaveSession(Callback):
+    def on_train_begin(self, logs={}):
+        self.best = 0
+        self.saver = tf.train.Saver()
+    
+    def on_epoch_end(self, logs={}):
+        acc = logs.get('val_acc')
+        if (acc >= self.best):
+            print(model.output.op.name)
+            self.saver.save(K.get_session(), '/keras_model.ckpt')
+
+save_session = SaveSession()
+
+
+# Train!
+model.fit_generator(generator=train_generator, steps_per_epoch=len(X_train), epochs=1, verbose=100, validation_data=(x_val, y_val), shuffle=True, callbacks=[save_model, save_session])
